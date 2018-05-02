fadd8773b81594d0150f23293cb378dc9428689d
Eero Saarinen <eiusaarinen@gmail.com>
2018-05-02 07:34:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
-def current_player(board)
+def turn_count(board)
   counter = 0
   
-  board.each do |index|
-    
+  board.each do |turn|
+    if (turn == "X") || (turn == "O")
+      counter += 1
+    end
+  end
+end
+
